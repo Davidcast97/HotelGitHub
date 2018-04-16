a38808fb2daadcd248cb2f5390c451147fb30ded
Davidcast97 <dfcastillov_1@uqvirtual.edu.co>
2018-04-15 23:29:34 -0500

    preCopia

---
+++ src/logica/Empleado.java
@@ -0,0 +1,73 @@
+package logica;
+
+public class Empleado {
+
+	private String nombre;
+	private String cedula;
+	private String telefono;
+	private String contraseña;
+
+	public Empleado(String nombre, String cedula, String telefono, String contraseña) {
+		this.nombre = nombre;
+		this.cedula = cedula;
+		this.telefono = telefono;
+		this.contraseña = contraseña;
+
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the cedula
+	 */
+	public String getCedula() {
+		return cedula;
+	}
+
+	/**
+	 * @param cedula the cedula to set
+	 */
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	/**
+	 * @return the telefono
+	 */
+	public String getTelefono() {
+		return telefono;
+	}
+
+	/**
+	 * @param telefono the telefono to set
+	 */
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	/**
+	 * @return the contraseña
+	 */
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	/**
+	 * @param contraseña the contraseña to set
+	 */
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+}
