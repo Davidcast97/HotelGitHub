e8419fcfb7719eb69e5eaf7dc7d872f4ce816fe3
Luis Felipe Tejada Padilla <lufepa@live.com>
2018-05-31 10:23:38 -0500

    vrecpe

--- src/interfaz/Principal/VentanaAdministrador.java
+++ src/interfaz/Principal/VentanaAdministrador.java
@@ -22,14 +22,14 @@ public class VentanaAdministrador extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private Hotel hotel2;
-	private JButton btnModHab, btnBalance, btnModificarRecepcionista, btnListarRecepcionistas,
-			btnModificarTienda, btnCrearRecepcionista;
+	private JButton btnModHab, btnBalance, btnModificarRecepcionista, btnListarRecepcionistas;
+	private JButton btnModificarTienda, btnCrearRecepcionista;
 	private JTextArea textArea;
 
 	public VentanaAdministrador() {
 		hotel2 =new Hotel();
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 603, 405);
+		setBounds(100, 100, 603, 323);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
